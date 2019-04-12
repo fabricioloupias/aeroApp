c44e7fe7bcc9fc622cb8ddb887227b8389d49f0c
Fabricio Loupias Scarfone <fabricio.scarfone@necxus.bsas>
2019-04-12 09:54:36 -0300

    aeroApp

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,7 @@
 
 export const environment = {
   production: false,
-  token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1Y2FiYmM4ZWEwZDM1ZjAwODI2MjBmZTkiLCJpYXQiOjE1NTQ3NTg3OTl9.ktA8LPxdj1O1dYucVrsJTGHkiiuDdtaczHfIMfjU5Bg',
-  url_api: 'https://aerolab-challenge.now.sh'
+  
 };
 
 /*
